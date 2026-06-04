80c9998f8bbc9b0fb42746e153d25e7966fd8063
lkolholk-ctrl <lkolholk@gmail.com>
2026-06-04 03:48:56 +0300

    fix: add missing imports for border and ImageVector

--- app/src/main/java/gs/git/vps/ui/screens/GitHubBranchProtectionModule.kt
+++ app/src/main/java/gs/git/vps/ui/screens/GitHubBranchProtectionModule.kt
@@ -2,6 +2,7 @@ package gs.git.vps.ui.screens
 
 import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
