83485aab719ab80168352d870c4233222fb65a4c
lkolholk-ctrl <lkolholk@gmail.com>
2026-06-06 17:57:11 +0300

    fix: add missing border import

--- app/src/main/java/gs/git/vps/ui/screens/GitHubNotificationsModule.kt
+++ app/src/main/java/gs/git/vps/ui/screens/GitHubNotificationsModule.kt
@@ -1,6 +1,7 @@
 package gs.git.vps.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
