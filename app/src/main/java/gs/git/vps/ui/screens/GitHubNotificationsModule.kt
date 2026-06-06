3b02fc4fe0194da31fbee33f7f08ab29b64f6649
lkolholk-ctrl <lkolholk@gmail.com>
2026-06-06 17:55:42 +0300

    fix: add missing background import

--- app/src/main/java/gs/git/vps/ui/screens/GitHubNotificationsModule.kt
+++ app/src/main/java/gs/git/vps/ui/screens/GitHubNotificationsModule.kt
@@ -1,5 +1,6 @@
 package gs.git.vps.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
